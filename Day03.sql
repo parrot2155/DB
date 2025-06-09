8a9751d3ad286ca530169ed46f35ac49a6f316f2
Park Tae Wook <parrot2155@gmail.com>
2025-06-09 13:08:36 +0900

    DAY3 1차

--- Day03.sql
+++ Day03.sql
@@ -57,10 +57,93 @@ FROM EMPLOYEE;
 
 
 
+--
+SELECT 
+	SYSDATE,
+	TO_CHAR(SYSDATE, 'RR/MM/DD'),
+	TO_CHAR(SYSDATE, 'YYYY-MM-DD HH:MI:SS'),
+	TO_CHAR(SYSDATE, 'YEAR, Q'), -- Q-> 분기. 
+	TO_CHAR(SYSDATE, 'MON, YYYY')
+FROM DUAL;
+
+
+--연도
+--Y/R
+SELECT 
+	TO_CHAR(TO_DATE('190325','YYMMDD'),'YYYY') "결과1",
+	TO_CHAR(TO_DATE('190325','RRMMDD'),'RRRR') "결과2",
+	TO_CHAR(TO_DATE('800325','YYMMDD'),'YYYY') "결과3",
+	TO_CHAR(TO_DATE('800325','RRMMDD'),'RRRR') "결과4"
+FROM DUAL;
+
+--4자리 한번에 입력받을 때는 문제 안됨. 
+--2자리 받는 경우
+--YY=> 현 세기 기준으로 값을 추가.
+--RR=> 반 세기
+
+--RR 50~99 -> 1900년대
+--	 00~49 -> 2000년대
+
+--YY 
+--80 -> 2080
+
+
+--SELECT 문의 실행 순서
+/*
+--SELECT 컬럼 AS 별칭, 계산식, 함수
+--FROM 테이블명 
+--WHERE 조건
+--GROUP BY 그룹을 묶을 컬럼명
+--HAVING 그룹에 대한 조건식, 함수식
+--ORDER BY 컬럼|별칭|순서 [ASC|DESC][, 컬럼명...]
+*/
+
+
+-- ORDER BY
+SELECT EMP_ID, EMP_NAME 이름, SALARY, DEPT_CODE FROM EMPLOYEE
+--ORDER BY 이름 DESC
+ORDER BY 4 DESC, EMP_NAME;
 
 
 
+--GROUP BY 그룹으로 묶는다.
 
+--부서별 평균 급여--
+--전체 사원의 급여 평균
+SELECT TRUNC(AVG(SALARY),-3) FROM EMPLOYEE;
+
+-- D1의 평균
+SELECT TRUNC(AVG(SALARY),-3) 
+FROM EMPLOYEE
+WHERE DEPT_CODE  = 'D6';
+
+
+--GROUP BY 절
+--특정 컬럼 , 계산식을 기준으로 
+--그룹별로 묶어 한 테이블 내에서 소그룹별로 조회하고자 할 때 선언하는 문구
+
+
+
+SELECT TRUNC(AVG(SALARY),-3) 
+FROM EMPLOYEE
+GROUP BY DEPT_CODE;
+
+
+--부서별 총 인원, 급여 합계, 급여 평균, 최대 급여, 최소 급여를 조회
+--단, 부서코드 기준으로 오름차순
+--급여 평균은 100의 자리 까지만 처리하고 나머지는 버림처리하자.
+
+SELECT COUNT(*), SUM(SALARY), TRUNC(AVG(SALARY),-2), MAX(SALARY), MIN(SALARY) 
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+ORDER BY DEPT_CODE ;
+
+
+--직급 코드 별 보너스 받는 사원의 수 조회
+SELECT JOB_CODE, COUNT(BONUS)
+FROM EMPLOYEE 
+GROUP BY JOB_CODE
+ORDER BY 1;
 
 
 
